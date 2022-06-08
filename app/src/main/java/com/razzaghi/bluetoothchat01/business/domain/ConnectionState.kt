366126f2ea0ce18c60e79bae895cffdced4326e0
razaghimahdi <razaghimahdi78@gmail.com>
2022-06-08 13:18:44 +0430

    - Added new state manger as ConnectionState

---
+++ app/src/main/java/com/razzaghi/bluetoothchat01/business/domain/ConnectionState.kt
@@ -0,0 +1,40 @@
+package com.razzaghi.bluetoothchat01.business.domain
+
+sealed class ConnectionState{
+
+    object None : ConnectionState() {
+        override fun toString(): String {
+            return "None"
+        }
+    }
+
+    object Inited : ConnectionState() {
+        override fun toString(): String {
+            return "Inited"
+        }
+    }
+
+    object Closed : ConnectionState() {
+        override fun toString(): String {
+            return "Closed"
+        }
+    }
+
+    object Failed : ConnectionState() {
+        override fun toString(): String {
+            return "Failed"
+        }
+    }
+
+    object Connected : ConnectionState() {
+        override fun toString(): String {
+            return "Connected"
+        }
+    }
+
+    object Wrote : ConnectionState() {
+        override fun toString(): String {
+            return "Wrote"
+        }
+    }
+}
