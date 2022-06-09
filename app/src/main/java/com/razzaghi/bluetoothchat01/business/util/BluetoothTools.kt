58d8c1d0f343938ddcb3666b89b5ab0cae90bafd
razaghimahdi <razaghimahdi78@gmail.com>
2022-06-09 12:30:07 +0430

    - Added new file as BluetoothTools.kt to manage some fun

---
+++ app/src/main/java/com/razzaghi/bluetoothchat01/business/util/BluetoothTools.kt
@@ -0,0 +1,36 @@
+package com.razzaghi.bluetoothchat01.business.util
+
+import android.annotation.SuppressLint
+import android.bluetooth.BluetoothAdapter
+import android.util.Log
+import com.razzaghi.bluetoothchat01.business.domain.DeviceData
+
+object BluetoothTools {
+
+    private val TAG = "AppDebug BluetoothTools"
+
+    @SuppressLint("MissingPermission")
+    fun getPairedDevices(bluetoothAdapter: BluetoothAdapter): List<DeviceData> {
+
+        Log.i(TAG, "getPairedDevices: ")
+
+        // Get a set of currently paired devices
+        val pairedDevices = bluetoothAdapter.bondedDevices
+        val mPairedDeviceList = arrayListOf<DeviceData>()
+        pairedDevices?.let { pairedDevices ->
+            // If there are paired devices, add each one to the ArrayAdapter
+            if (pairedDevices.isNotEmpty()) {
+                // There are paired devices. Get the name and address of each paired device.
+                for (device in pairedDevices) {
+                    Log.i(TAG, "getPairedDevices device: "+device.toString())
+                    val deviceName = device.name
+                    val deviceHardwareAddress = device.address // MAC address
+                    mPairedDeviceList.add(DeviceData(deviceName, deviceHardwareAddress))
+                }
+            }
+        }
+        Log.i(TAG, "getPairedDevices mPairedDeviceList: "+mPairedDeviceList)
+
+        return mPairedDeviceList
+    }
+}
