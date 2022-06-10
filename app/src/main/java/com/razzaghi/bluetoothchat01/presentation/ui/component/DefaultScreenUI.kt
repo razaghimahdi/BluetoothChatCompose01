e7ca9c2c3f0dd8cf0f924f34589d66fc4c3dace9
razaghimahdi <razaghimahdi78@gmail.com>
2022-06-10 12:59:59 +0430

    - Added new compose widget to show loading

--- app/src/main/java/com/razzaghi/bluetoothchat01/presentation/ui/component/DefaultScreenUI.kt
+++ app/src/main/java/com/razzaghi/bluetoothchat01/presentation/ui/component/DefaultScreenUI.kt
@@ -2,20 +2,26 @@ package com.razzaghi.bluetoothchat01.presentation.ui.component
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.LayoutDirection
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun DefaultScreenUI(
+    isLoading: Boolean,
     content: @Composable () -> Unit,
 ) {
 
@@ -29,6 +35,10 @@ fun DefaultScreenUI(
                     .background(MaterialTheme.colors.background)
             ) {
                 content()
+
+                if (isLoading) {
+                    CircularIndeterminateProgressBar()
+                }
             }
         }
  }
@@ -38,6 +48,19 @@ fun DefaultScreenUI(
 
 
 
+@Composable
+fun CircularIndeterminateProgressBar() {
+    Row(
+        modifier = Modifier.fillMaxSize(),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ){
+        CircularProgressIndicator(
+            modifier = Modifier,
+            color = MaterialTheme.colors.primary
+        )
+    }
+}
 
 
 
