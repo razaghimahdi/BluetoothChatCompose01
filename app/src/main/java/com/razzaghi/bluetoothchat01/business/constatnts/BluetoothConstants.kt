c83edd42ea4d01321d0def6e89b267f003d291f5
razaghimahdi <razaghimahdi78@gmail.com>
2022-06-12 09:45:31 +0430

    - Added new constants value

--- app/src/main/java/com/razzaghi/bluetoothchat01/business/constatnts/BluetoothConstants.kt
+++ app/src/main/java/com/razzaghi/bluetoothchat01/business/constatnts/BluetoothConstants.kt
@@ -18,9 +18,11 @@ object BluetoothConstants {
     const val BLUETOOTH_SOCKET_TYPE_SECURE = "Secure"
     const val BLUETOOTH_SOCKET_TYPE_INSECURE = "Insecure"
 
-      val DEFAULT_OUTPUT_STREAM =  ByteArrayOutputStream(1024)
-      val DEFAULT_INPUT_STREAM =  ByteArrayInputStream(ByteArray(1024))
+    val DEFAULT_OUTPUT_STREAM = ByteArrayOutputStream(1024)
+    val DEFAULT_INPUT_STREAM = ByteArrayInputStream(ByteArray(1024))
 
+    const val MESSAGE_TYPE_SENT = 0
+    const val MESSAGE_TYPE_RECEIVED = 1
 
 
 }
