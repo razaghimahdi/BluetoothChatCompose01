154fe376782627ffc7c3a65cdeb6d652d53644de
razaghimahdi <razaghimahdi78@gmail.com>
2022-06-12 09:47:15 +0430

    - Added MessageTools to add some fun for Message

---
+++ app/src/main/java/com/razzaghi/bluetoothchat01/business/util/MessageTools.kt
@@ -0,0 +1,26 @@
+package com.razzaghi.bluetoothchat01.business.util
+
+import com.razzaghi.bluetoothchat01.business.constatnts.BluetoothConstants.MESSAGE_TYPE_RECEIVED
+import com.razzaghi.bluetoothchat01.business.constatnts.BluetoothConstants.MESSAGE_TYPE_SENT
+import java.text.SimpleDateFormat
+import java.util.*
+
+object MessageTools {
+
+    fun Int.convertToString():String{
+        if (this == MESSAGE_TYPE_SENT) return "You"
+        if (this == MESSAGE_TYPE_RECEIVED) return "User"
+        return "Unknown"
+    }
+
+
+    fun Long.convertToString(): String {
+        val df: SimpleDateFormat = SimpleDateFormat("hh:mm a", Locale.getDefault())
+
+        val resultdate = Date(this)
+
+        return df.format(resultdate)
+    }
+
+
+}
